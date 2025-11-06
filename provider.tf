bcfdad45df71595b1330f22da4a2e3bb9fb591fe
shraavya <shraavya2002@gmail.com>
2025-11-06 11:07:07 +0000

    terraform cloud integration

---
+++ provider.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "shraavya-tf-stroage" #change it with your rg group for the storage account you created
+    storage_account_name = "shraavyatfstorage" #change it with your storage account name
+    container_name = "tfstate"  #change it with your container name
+    key = "project1.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
+
+variable "client_id" {
+  type = string
+}
+ 
+variable "client_secret" {
+  type = string
+}
+ 
+variable "subscription_id" {
+  type = string
+}
+ 
+variable "tenant_id" {
+  type = string
+}
